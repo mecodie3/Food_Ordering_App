b500413e2202c465b52cb7a9c7de1525e09cb56e
redroze <mecodie3@gmail.com>
2025-09-17 23:04:58 +0100

    Adding Dynamic Links

--- restaurant_menu/urls.py
+++ restaurant_menu/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 #string empty for homepage
 urlpatterns = [
-    path('',views.MenuList.as_view(),name='home')
+    path('',views.MenuList.as_view(),name='home'),
+    path('item/<int:pk>/', views.MenuItemDetail.as_view(), name="menu_item")
 ]
